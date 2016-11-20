4fd4a61b710b289d389450a0cf2adb8d859ea6cc
PaulMcGonagle <mcgonagle.paul@gmail.com>
2016-11-20 19:25:36 +0000

    Added time tests to Event

--- Calendar.Test/EventTests.cs
+++ Calendar.Test/EventTests.cs
@@ -23,7 +23,7 @@ namespace Calendar.Test
             {
                 var fakeClock = ScheduleTestHelper.GetFakeClock(2016, 05, 01);
 
-                this.WithExamples(new ExampleTable("sut", "expectedWeekday")
+                this.WithExamples(new ExampleTable("sut", "expectedWeekday", "expectedStartTime", "expectedEndTime")
                     {
                         {
                             new Event()
@@ -46,9 +46,11 @@ namespace Calendar.Test
                                             TimeZoneProvider = TimeZoneProvider,
                                         }
                                     }
-                                }
+                                },
                             },
-                            IsoDayOfWeek.Thursday
+                            IsoDayOfWeek.Thursday,
+                            new LocalTime(16, 45),
+                            new LocalTime(17, 30)
                         }
                     })
                     .BDDfy();
@@ -67,6 +69,75 @@ namespace Calendar.Test
             public void ThenAllSerialsHaveTheCorrectWeekday(IsoDayOfWeek expectedWeekday)
             {
                 _serials.Episodes.Select(e => e.From.DayOfWeek).ShouldAllBe(d => d.Equals((int)expectedWeekday));
+            }
+
+            public void ThenAllStartTimesAresCorrect(LocalTime expectedStartTime)
+            {
+                _serials.Episodes.Select(e => e.From.TimeOfDay).ShouldAllBe(d => d.Equals(expectedStartTime));
+            }
+
+            public void ThenAllEndTimesAreCorrect(LocalTime expectedEndTime)
+            {
+                _serials.Episodes.Select(e => e.End.TimeOfDay).ShouldAllBe(d => d.Equals(expectedEndTime));
+            }
+        }
+        public class VerifyTimeInOtherTimeZone
+        {
+            private Event _sut;
+            private ISerial _serials;
+
+            private const string TimeZoneProvider = "Europe/London";
+
+            [Fact]
+            public void Execute()
+            {
+                var fakeClock = ScheduleTestHelper.GetFakeClock(2016, 05, 01);
+
+                this.WithExamples(new ExampleTable("sut", "expectedStartTime", "expectedEndTime")
+                    {
+                        {
+                            new Event()
+                            {
+                                Title = "Street dance",
+                                Serials = new Serials
+                                {
+                                    new Serial
+                                    {
+                                        Schedule = 
+                                            new SingleDay
+                                            {
+                                                Date = new LocalDate(2016, 07, 01),
+                                            },
+                                        TimeStart = new LocalTime(14, 00),
+                                        Period = new PeriodBuilder { Minutes = 1 }.Build(),
+                                        TimeZoneProvider = "Europe/London",
+                                    }
+                                },
+                            },
+                            new LocalTime(14, 00),
+                            new LocalTime(14, 45)
+                        }
+                    })
+                    .BDDfy();
+            }
+
+            public void GivenSut(Event sut)
+            {
+                _sut = sut;
+            }
+
+            public void WhenSomethingIsDone()
+            {
+                _serials = _sut.Serials;
+            }
+
+            public void ThenAllStartTimesAresCorrect(LocalTime expectedStartTime)
+            {
+                _serials.Episodes.Select(e => e.From.TimeOfDay).ShouldAllBe(d => d.Equals(expectedStartTime));
+            }
+
+            public void ThenAllEndTimesAreCorrect(LocalTime expectedEndTime)
+            {
             }
         }
     }
