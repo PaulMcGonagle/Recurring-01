b32293a4c45552e6cdd7fba9001f7c843f8a548e
Paul McGonagle <mcgonagle.paul@gmail.com>
2017-11-08 03:53:50 +0000

    Add basic GenerateFromVertex

---
+++ Generators/Instances/GenerateFromSchedule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NodaTime;
+using Scheduler;
+using Scheduler.Persistance;
+
+namespace Generators.Instances
+{
+    public class GenerateFromSchedule : IGenerateFromVertex
+    {
+        public IEnumerable<IVertex> Generate(IVertex vertex, IClock clock)
+        {
+            ISchedule schedule = vertex as ISchedule;
+
+            if (schedule == null)
+            {
+                throw new Exception($"Expected ISchedule. Vertex was type {vertex.GetType()}");
+            }
+
+            6
+        }
+    }
+}
