e594c94b15fec10db81d3dd37a7859fbac3c8c9f
Paul McGonagle <mcgonagle.paul@gmail.com>
2017-11-10 00:55:11 +0000

    Remove stuff. Add scenarios.

---
+++ Scenarios/School/Generate.cs
@@ -0,0 +1,23 @@
+﻿using ArangoDB.Client;
+using Generators;
+using NodaTime;
+
+namespace School
+{
+    public class Generate
+    {
+        public static void Go(IArangoDatabase db, IClock clock)
+        {
+            var generator = GeneratorFactory.Get("calendar");
+
+            var generated = generator.Generate(
+                "C:\\Users\\mcgon\\Source\\Repos\\Recurring-01\\Scenarios\\School\\Files\\Years.xml",
+                clock);
+
+            foreach (var g in generated)
+            {
+                g.Save(db, clock);
+            }
+        }
+    }
+}
